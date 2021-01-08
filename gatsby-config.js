8fd96a16c363da0fb048ee5483b333f103f0837a
Jonathan <sir15004@byui.edu>
2021-01-07 19:40:55 -0800

    color and meta changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Tieguy1208 | Lego Guns',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'A gallery website for Tieguys1208\'s Lego creations',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
